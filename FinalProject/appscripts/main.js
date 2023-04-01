a96e81831f9b5b8aac3f0cef66db2c1840d486a5
kduby <kew005@ucsd.edu>
2023-04-01 13:20:06 +0800

    changing chart details

--- FinalProject/appscripts/main.js
+++ FinalProject/appscripts/main.js
@@ -1,47 +1,45 @@
 // Our labels along the x-axis
-var years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
+var date = [22, 23, 24, 25, 26];
 // For drawing the lines
-var africa = [86,114,106,106,107,111,133,221,783,2478];
-var asia = [282,350,411,502,635,809,947,1402,3700,5267];
-var europe = [168,170,178,190,203,276,408,547,675,734];
-var latinAmerica = [40,20,10,16,24,38,74,167,508,784];
-var northAmerica = [6,3,2,2,7,26,82,172,312,433];
+var past = [5,5,5,3,1];
+var present = [8,6,2,9,2];
+var future = [10,10,10,1,1];
+var customized = [10,10,10,10,10];
 
 var myChart = new Chart("myChart", {
     type: 'line',
     data: {
-      labels: years,
+      labels: date,
       datasets: [
         { 
-          data: africa,
-          label: "Africa",
+          data: past,
+          label: "past",
           borderColor: "#3e95cd",
           fill: false
         },
         { 
-          data: asia,
-          label: "Asia",
+          data: present,
+          label: "present",
           borderColor: "#8e5ea2",
           fill: false
         },
         { 
-          data: europe,
-          label: "Europe",
+          data: future,
+          label: "future",
           borderColor: "#3cba9f",
           fill: false
         },
         { 
-          data: latinAmerica,
-          label: "Latin America",
+          data: customized,
+          label: "customized",
           borderColor: "#e8c3b9",
-          fill: false
-        },
-        { 
-          data: northAmerica,
-          label: "North America",
-          borderColor: "#c45850",
           fill: false
         }
       ]
     }
   });
+
+  var now = new Date().toDateString();
+  console.log(now);
+
+  document.getElementById("time").textContent = now;
